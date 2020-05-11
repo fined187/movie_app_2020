8be5bcef20acb274c13f0ebb8923ec6b9a747238
woo taik lee <wootaiklee@wooui-MacBookPro.local>
2020-05-11 15:16:14 +0900

    Updates

---
+++ precache-manifest.855f0560ce093dcc304e26c2abbb4d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a741bf90c6a7617c731cfcdb8fb50e3c",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "5754b202850a64cb46f4",
+    "url": "/movie_app_2020/static/css/main.6bb53541.chunk.css"
+  },
+  {
+    "revision": "261fc6083f1d5d248b8b",
+    "url": "/movie_app_2020/static/js/2.769f61a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.769f61a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5754b202850a64cb46f4",
+    "url": "/movie_app_2020/static/js/main.f4b35965.chunk.js"
+  },
+  {
+    "revision": "9927c9d5c16bc5ac0d63",
+    "url": "/movie_app_2020/static/js/runtime-main.72517e8e.js"
+  }
+]);
